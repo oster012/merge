af794b048d8b16278a2f802608cced9a877444fa
profile <oster012@maclab-81.uwp.edu>
2018-04-20 15:47:22 -0500

    Initial Commit

---
+++ mergeme/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mergeme
+//
+//  Created by Ostergaard, Don James W (oster012) on 4/20/18.
+//  Copyright © 2018 Ostergaard, Don James W (oster012). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
