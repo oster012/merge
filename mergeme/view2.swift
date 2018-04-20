99f385ea30ad251b2f5d07b8696239f9d666bc05
profile <oster012@maclab-81.uwp.edu>
2018-04-20 16:00:49 -0500

    cgsd

---
+++ mergeme/view2.swift
@@ -0,0 +1,13 @@
+//
+//  view2.swift
+//  mergeme
+//
+//  Created by Ostergaard, Don James W (oster012) on 4/20/18.
+//  Copyright © 2018 Ostergaard, Don James W (oster012). All rights reserved.
+//
+
+import UIKit
+
+class view2: NSObject {
+
+}
